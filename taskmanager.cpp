2a5d014178378f147d16a8c70a6aebb30df942fc
John-Holik <johnholikiii@outlook.com>
2024-10-28 18:06:22 -0400

    Added loop to allow multiple actions during program execution

--- taskmanager.cpp
+++ taskmanager.cpp
@@ -42,19 +42,48 @@ void removeTasks(){
 
 }//end removeTasks
 
+bool continueInput(){
+    char doContinue;
+    std::cout << "Would you like to continue? (y/n): ";
 
+    // Check if input is valid
+    bool validInput = false;
+    do{
+        std::cin >> doContinue;
+        if (doContinue == 'y' || doContinue == 'Y' || doContinue == 'n' || doContinue == 'N'){
+            validInput = true;
+        }
+        else{
+            std::cout << "Invalid Input. Please enter 'y' or 'n': ";
+        }
+
+    } while (!validInput);
+    
+    // If they do not want to continue they will enter 'n' and the program loop will stop
+    if (doContinue == 'n' || doContinue == 'N'){
+        return false;
+    }else{ // otherwise they entered 'y' and they want to do another action
+        return true;
+    }
+    
+}//end continueInput
 
 
 // Main
 // *********************************************************************************
 int main(){
-    // Let the User Choose their action:
-    int input = -1;
-    input = chooseTask();
+    // Create a loop to let the user switch between actions after completion
+    bool keepRunning = true;
 
-    switch(input){
+    do{
+        // Let the User Choose their action:
+        int input = -1;
+        input = chooseTask();
+
+        switch (input)
+        {
         // View tasks in file
-        case 1: 
+        case 1:
             viewTasks();
             break;
         // Add tasks to file
@@ -65,7 +94,13 @@ int main(){
         case 3:
             removeTasks();
             break;
-    }
+        }
+        
+        // After action is completed, see if user wants to continue or exit the program
+        keepRunning = continueInput();
+
+    }while(keepRunning);
+    
 
     return 0;
 }// end main
